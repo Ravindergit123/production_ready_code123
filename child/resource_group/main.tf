abed8f917017e0f1e45f2d722be50035e5c6bc96
Ravinder Kumar <ravinderkumarairtel4@gmail.com>
2026-08-09 19:42:40 +0530

    Fix TFLint unused declarations for local.tags and local.subnets

--- child/resource_group/main.tf
+++ child/resource_group/main.tf
@@ -1,7 +1,6 @@
 resource "azurerm_resource_group" "ravi" {
-    for_each = var.rg_name
-    name     = each.key
-    location = each.value
-  
+  for_each = var.rg_name
+  name     = each.key
+  location = each.value
+  tags     = local.tags
 }
-
--- child/subnet/main.tf
+++ child/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "rg_subnet" {
-  for_each             = var.rg_subnet
+  for_each             = local.subnets
   name                 = each.value.name
   resource_group_name  = each.value.resource_group_name
   virtual_network_name = each.value.virtual_network_name
