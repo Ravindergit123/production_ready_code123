c3ca8393c0f2e11571a360c03110540f197074f7
Ravindergit123 <ravinderkumarzte@gmail.com>
2026-08-15 21:53:34 +0530

    Fix resource group resource definition in main.tf

--- child/resource_group/main.tf
+++ child/resource_group/main.tf
@@ -4,3 +4,10 @@ resource "azurerm_resource_group" "ravi" {
   location = each.value
   tags     = local.tags
 }
+
+resource "azurerm_resource_group" "ravi" {
+  for_each = var.rg_name
+  name     = each.key
+  location = each.value
+  tags     = local.tags
+}
