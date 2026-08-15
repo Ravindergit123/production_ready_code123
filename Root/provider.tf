b176ed8c06706ad5c1ffcf7021f0e43cf76b783d
Ravinder Kumar <ravinderkumarairtel4@gmail.com>
2026-08-15 22:19:31 +0530

    Add required_version to all terraform blocks

--- Root/provider.tf
+++ Root/provider.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 1.0"
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.76.0"
+      version = "~> 4.0"
     }
   }
 }
