5ce9642f5de0e9d00ac583ce42e33207ae3989b2
Ravinder Kumar <ravinderkumarairtel4@gmail.com>
2026-08-15 08:19:26 +0530

    code delete changes

--- child/resource_group/main.tf
+++ child/resource_group/main.tf
@@ -25,3 +25,16 @@ resource "azurerm_resource_group" "ravi" {
   location = each.value
   tags     = local.tags
 }
+resource "azurerm_resource_group" "ravi" {
+  for_each = var.rg_name
+  name     = each.key
+  location = each.value
+  tags     = local.tags
+}
+
+# resource "azurerm_resource_group" "ravi" {
+#   for_each = var.rg_name
+#   name     = each.key
+#   location = each.value
+#   tags     = local.tags
+# }
