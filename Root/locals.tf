9e5d2f27333c520b45ad69dc369c2ca7023f741f
Ravinder Kumar <ravinderkumarairtel4@gmail.com>
2026-08-16 08:47:16 +0530

    Remove unused locals from Root module

--- Root/locals.tf
+++ Root/locals.tf
@@ -1,11 +1,2 @@
 locals {
-  environment = var.environment
-  common_tags = merge(
-    var.tags,
-    {
-      Environment = var.environment
-      ManagedBy   = "Terraform"
-      Project     = "Azure-Infra"
-    }
-  )
 }
