1f39ce975e61eeddf742d9fa4847b25a346c4059
Ravinder Kumar <ravinderkumarairtel4@gmail.com>
2026-08-16 11:09:58 +0530

    Enable remote azurerm backend in backend.tf to maintain state across CI/CD runs

--- Root/backend.tf
+++ Root/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  # backend "azurerm" {
-  #   resource_group_name  = "tfstate-rg"
-  #   storage_account_name = "ravitfstate2026"
-  #   container_name       = "tfstate"
-  #   key                  = "prod.terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "ravitfstate2026"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
 }
