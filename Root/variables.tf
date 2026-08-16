b4bad958e3f7b15acfb8c3fdef42cf3c33b42519
Ravindergit123 <ravinderkumarzte@gmail.com>
2026-08-16 08:51:33 +0530

    Remove unused variables (environment and tags) to fix TFLint warnings

--- Root/variables.tf
+++ Root/variables.tf
@@ -1,15 +1,3 @@
-variable "environment" {
-  description = "Target deployment environment"
-  type        = string
-  default     = "prod"
-}
-
-variable "tags" {
-  description = "Global tags applied to all resources"
-  type        = map(string)
-  default     = {}
-}
-
 variable "rg_name" {
   description = "Map of Resource Groups to create"
   type        = map(string)
